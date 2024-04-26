2e613790aeca0ed688c48d09c4fba52d24b796ad
AJ <ajw53@NJIT.edu>
2024-04-26 15:35:25 -0400

    Fix  show rows and columns spacing Add inquiry section to homepage

--- shows.js
+++ shows.js
@@ -1,25 +1,40 @@
 $(document).ready(function() {
     $.getJSON('./getShows.php')
-        .done(function(shows) {
-            if (shows.length === 0) {
-                var html = '<p>No upcoming shows</p>';
-            } else {
-                var html = '';
-                $.each(shows, function(key, show) {
-                    html += '<div class="row show border-bottom mb-3">';
-                    html += '<div class="col-md mb-3 d-flex align-items-start align-items-md-center justify-content-start justify-content-md-start"><p class="show-date mb-0">' + show.date + '</p></div>';
-                    html += '<div class="col-md ml-md-auto mb-3 d-flex align-items-start align-items-md-center justify-content-start justify-content-md-center"><p class="show-venue mb-0">' + show.venue + ' @ ' + show.time + '</p></div>';
-                    html += '<div class="col-md ml-md-auto mb-3 d-flex align-items-start align-items-md-center justify-content-start justify-content-md-end"><p class="show-location mb-0 mr-3">' + show.city + '</p>';
-                    if (show.ticket_link) {
-                        html += '<a href="' + show.ticket_link + '" class="btn btn-outline-light buyTix">Buy Tickets</a>';
-                    }
-                    html += '</div>';
-                    html += '</div>';
-                });
-            }
-            $('#showsContainer').html(html);
+        .done(function(data) {
+            var upcomingShows = data.upcoming;
+            var pastShows = data.past;
+
+            var upcomingHtml = '';
+            var pastHtml = '';
+
+            $.each(upcomingShows, function(key, show) {
+                // This is an upcoming show
+                upcomingHtml += '<div class="row show border-bottom mb-3">';
+                upcomingHtml += '<div class="col-md-2 mb-3 d-flex align-items-start align-items-md-center justify-content-start justify-content-md-start"><p class="show-date mb-0 show-text-large">' + show.date + '</p></div>';
+                upcomingHtml += '<div class="col-md-6 mb-3 d-flex align-items-start align-items-md-center justify-content-start justify-content-md-left"><p class="show-venue mb-0 show-text-large">' + show.venue + ' @ ' + show.time + '</p></div>';
+                upcomingHtml += '<div class="col-md-4 mb-3 d-flex align-items-start align-items-md-center justify-content-start justify-content-md-end"><p class="show-location mb-0 mr-3 show-text-large">' + show.city + '</p>';
+                if (show.ticket_link) {
+                    upcomingHtml += '<a href="' + show.ticket_link + '" class="btn btn-outline-light buyTix">Buy Tickets</a>';
+                }
+                upcomingHtml += '</div></div>';
+            });
+            
+            $.each(pastShows, function(key, show) {
+                // This is a past show
+                pastHtml += '<div class="row show border-bottom mb-3">';
+                pastHtml += '<div class="col-md-2 mb-3 d-flex align-items-start align-items-md-center justify-content-start justify-content-md-start"><p class="show-date mb-0 show-text-large">' + show.date + '</p></div>';
+                pastHtml += '<div class="col-md-6 mb-3 d-flex align-items-start align-items-md-center justify-content-start justify-content-md-left"><p class="show-venue mb-0 show-text-large">' + show.venue + ' @ ' + show.time + '</p></div>';
+                pastHtml += '<div class="col-md-4 mb-3 d-flex align-items-start align-items-md-center justify-content-start justify-content-md-end"><p class="show-location mb-0 mr-3 show-text-large">' + show.city + '</p>';
+                if (show.ticket_link) {
+                    pastHtml += '<a href="' + show.ticket_link + '" class="btn btn-outline-light buyTix">Buy Tickets</a>';
+                }
+                pastHtml += '</div></div>';
+            });
+
+            $('#upcomingShowsContainer').html(upcomingHtml);
+            $('#pastShowsContainer').html(pastHtml);
         })
         .fail(function(jqXHR, textStatus, errorThrown) {
-            $('#showsContainer').html('<p>An error occurred while loading the shows: ' + errorThrown + '</p>');
+            $('#upcomingShowsContainer').html('<p>An error occurred while loading the shows: ' + errorThrown + '</p>');
         });
 });
